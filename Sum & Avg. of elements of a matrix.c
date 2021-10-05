f57f19d37dbdbe56b3d7d342e06c6ed72b1da604
HARSH MISHRA <71396592+hm3478@users.noreply.github.com>
2021-10-05 09:42:23 +0530

    increased the readability of the code
    Added comments and increased the readability of the code

--- Sum & Avg. of elements of a matrix.c
+++ Sum & Avg. of elements of a matrix.c
@@ -2,20 +2,27 @@
 int main()
 {
 	int i,j,m,n,sum=0,avg=0;
-	printf("Enter Number Of Rows : ");
-	scanf("%d",&m);
-	printf("Enter Number Of Columns : ");
-	scanf("%d",&n);
-	int matrix[m][n];
-	printf("Enter Matrix Elements : ");
-	for(i=0;i<m;i++)
+        int matrix[m][n];         //declaring all the variables
+	
+        printf("Enter Number Of Rows");
+	scanf("%d",&m);  
+	
+        printf("Enter Number Of Columns : ");
+	scanf("%d",&n);                //taking input in form of rows and columns
+	
+	printf("Enter Matrix Elements");
+	
+         for(i=0;i<m;i++)
 		for(j=0;j<n;j++)
-			scanf("%d",&matrix[i][j]);		
-	for(i=0;i<m;i++)
+			scanf("%d",&matrix[i][j]);    	
+	
+          for(i=0;i<m;i++)
 		for(j=0;j<n;j++)
-			sum=sum+matrix[i][j];
-	avg=sum/(m*n);
-	printf("\nSum Of All Elements In Matrix = %d",sum);
+			sum=sum+matrix[i][j];   //calculating sum
+	
+        avg=sum/(m*n);   //calculating average
+	
+        printf("\nSum Of All Elements In Matrix = %d",sum);
 	printf("\nAverage Of Matrix = %d",avg);
 	return 0;
 }
